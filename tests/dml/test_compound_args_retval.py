0c4872f6edbc4801c9eb08dc286efaf205054d8d
Marco Cusumano-Towner <imarcoam@gmail.com>
2021-09-22 14:03:53 -0400

    add support for AD with namedtuples

--- tests/dml/test_compound_args_retval.py
+++ tests/dml/test_compound_args_retval.py
@@ -4,12 +4,14 @@ from pygen.choice_address import addr
 
 import torch
 
+from collections import namedtuple
+Foo = namedtuple('Foo', ['x', 'y', 'z'])
 
 @gendml
 def f1(x, y, compound):
     assert isinstance(compound, dict)
     (a, b, (c, d, (e, g))) = compound['k1']
-    return [(x, y), (x, [x, y]), {'k2': a + b + c + d + e + g}]
+    return [(x, y), (x, [x, y]), {'k2': a + b + c + d + e + g}, Foo(x, x, y)]
 
 
 def test_choice_gradients_no_calls():
@@ -17,9 +19,9 @@ def test_choice_gradients_no_calls():
     x = y = a = b = c = d = e = g = t(0.0)
     compound = {'k1': (a, b, [c, d, (e, g)])}
     trace = f1.simulate((x, y, compound))
-    retgrad = [(t(1.0), t(1.0)), (t(1.0), [t(1.0), t(1.0)]), {'k2': t(1.0)}]
-    expected_x_grad = t(3.0)
-    expected_y_grad = t(2.0)
+    retgrad = [(t(1.0), t(1.0)), (t(1.0), [t(1.0), t(1.0)]), {'k2': t(1.0)}, Foo(t(1.1), t(1.2), t(1.3))]
+    expected_x_grad = t(3.0) + t(1.1) + t(1.2)
+    expected_y_grad = t(2.0) + t(1.3)
     expected_abcdeg_grad = t(1.0)
     (arg_grads, choice_values, choice_grads) = trace.choice_gradients(None, retgrad)
     assert len(arg_grads) == 3
@@ -49,10 +51,10 @@ def test_choice_gradients_calls():
     x = y = a = b = c = d = e = g = t(0.0)
     compound = {'k1': (a, b, [c, d, (e, g)])}
     trace = f2.simulate((x, y, compound))
-    retgrad = [(t(1.0), t(1.0)), (t(1.0), [t(1.0), t(1.0)]), {'k2': t(1.0)}]
+    retgrad = [(t(1.0), t(1.0)), (t(1.0), [t(1.0), t(1.0)]), {'k2': t(1.0)}, Foo(t(1.1), t(1.2), t(1.3))]
     retgrad = (retgrad, retgrad)
-    expected_x_grad = t(3.0) * 2  # 0
-    expected_y_grad = t(2.0) * 2 # 5
+    expected_x_grad = (t(3.0) + t(1.1) + t(1.2))* 2
+    expected_y_grad = (t(2.0) + t(1.3)) * 2
     expected_abcdeg_grad = t(1.0) * 2
     (arg_grads, choice_values, choice_grads) = trace.choice_gradients(None, retgrad)
     print(arg_grads)
